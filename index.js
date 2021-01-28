a82c4fd96fb7e51fc4caa76d0a0d6bc6e7c52742
kautukdwivedi <kautukdwivedi@icloud.com>
2021-01-28 21:42:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+        const play = document.querySelector('button');
+
+        const svgContainer = document.getElementById('svg');
+        const animItem = bodymovin.loadAnimation({
+            wrapper: svgContainer,
+            animType: 'svg',
+            loop: false,
+            autoplay: false,
+            path: 'https://assets2.lottiefiles.com/packages/lf20_u4yrau.json'
+        });
+
+        play.addEventListener('click', () => {
+            svgContainer.classList.remove('hide');
+            animItem.goToAndPlay(0,true);
+        })
+
+        animItem.addEventListener('complete', () => {
+            svgContainer.classList.add('hide');
+            
+        })
